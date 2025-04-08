9e1e7ea33e004702267ddef6622ff0ee71af9f2c
Shahriar Hossain <66290875+Shahriar997@users.noreply.github.com>
2025-04-09 03:33:06 +0600

    added employer account feature

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\MyJobController;
@@ -19,10 +20,13 @@ Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])
     ->name('auth.destroy');
 
-Route::middleware('auth')->group(function() {
+Route::middleware('auth')->group(function () {
     Route::resource('job.application', JobApplicationController::class)
         ->only(['create', 'store']);
-        
+
     Route::resource('my-job-application', MyJobController::class)
         ->only(['index', 'destroy']);
+
+    Route::resource('employer', EmployerController::class)
+        ->only(['create', 'store']);
 });
